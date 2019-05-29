f79941a6c9495ae74adb937af3eb13127a6d446f
Fernando Oliveira <Fernando.Nascimento.Oliveira03@gmail.com>
2019-05-29 17:56:51 -0300

    Whole files

---
+++ functions.js
@@ -0,0 +1,49 @@
+/*  functions para produtos */    
+
+    function mudafoto3() {
+       document.getElementById('P1').src = 'produtos/p1hover.jpg';
+    }
+
+    function voltafoto1() {
+        document.getElementById('P1').src = 'produtos/p1.jpg';
+    }
+
+    function mudafoto4() {
+        document.getElementById('p2').src = 'produtos/p2hover.jpg';
+    }
+
+    function voltafoto2() {
+        document.getElementById('p2').src = 'produtos/p2.jpg';
+    }
+
+    function mudafoto5() {
+        document.getElementById('p3').src = 'produtos/p3hover.jpg';
+    }
+
+    function voltafoto3() {
+        document.getElementById('p3').src = 'produtos/p3.jpg';
+    }
+
+    function mudafoto6() {
+        document.getElementById('P4').src = 'produtos/p4hover.jpg';
+    }
+
+    function voltafoto6() {
+        document.getElementById('P4').src = 'produtos/p4.jpg';
+    }
+
+    function mudafoto7() {
+        document.getElementById('P5').src = 'produtos/p5hover.jpg';
+    }
+
+    function voltafoto7() {
+        document.getElementById('P5').src = 'produtos/p5.jpg';
+    }
+
+        function mudafoto8() {
+        document.getElementById('P6').src = 'produtos/p6hover.jpg';
+    }
+
+    function voltafoto8(){
+        document.getElementById('P6').src = 'produtos/p6.jpg';
+    }
